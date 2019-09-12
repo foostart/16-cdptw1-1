d6dadbbe93a1685338451a1a56154e9b4cd68c1b
foostart.com <foostart.com@gmail.com>
2019-09-12 14:17:56 +0700

    test

---
+++ 702/702-content.php
@@ -0,0 +1,119 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="container container-modify header">
+    <div class="modul-702"> 
+        <div class="col-md-6 col-sm-6 col-xs-12 images-container">
+            <div class="img-container">
+                <div class="col-md-9 col-sm-9 col-xs-9 swiper-container gallery-top image-block">
+                    <div id="view_full_size" class="swiper-wrapper">
+                        <div class="zoomPad swiper-slide" id="img1">
+                            <img class="img-responsive" src="<?php echo $url_path ?>/images/large1.jpg" alt="">
+                        </div>
+                        <div class="zoomPad swiper-slide " id="img2">
+                            <img class="img-responsive" src="<?php echo $url_path ?>/images/large2.jpg" alt="">
+                        </div>
+                        <div class="zoomPad swiper-slide">
+                            <img class="img-responsive" src="<?php echo $url_path ?>/images/large3.jpg" alt="">
+                        </div>
+                        <div class="zoomPad swiper-slide">
+                            <img class="img-responsive" src="<?php echo $url_path ?>/images/large4.jpg" alt="">
+                        </div>
+                    </div>
+                </div> 
+                <div class="col-md-3 col-sm-3 col-xs-3 swiper-container gallery-thumbs views-block">   
+                    <div class="swiper-wrapper thumbs-list">
+                        <div class="swiper-slide thumb-list-frame">
+                            <a href="#img1" data-toggle="tab">
+                            <img class="img-responsive" src="<?php echo $url_path ?>/images/large1.jpg">    
+                            </a>
+                        </div>
+                        <div class="swiper-slide thumb-list-frame">
+                            <a href="#img2" data-toggle="tab">
+                            <img class="img-responsive" src="<?php echo $url_path ?>/images/large2.jpg" >   
+                            </a>
+                        </div>
+                        <div class="swiper-slide thumb-list-frame">
+                            <img class="img-responsive" src="<?php echo $url_path ?>/images/large3.jpg">    
+                        </div>
+                        <div class="swiper-slide thumb-list-frame">
+                            <img class="img-responsive" src="<?php echo $url_path ?>/images/large4.jpg">    
+                        </div>
+                    </div>
+                </div>  
+                <div class="clearfix"></div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="carousel slide multi-item-carousel" id="theCarousel">
+                <div class="productscategory_h2">Related products</div>
+                <div class="carousel-margin">
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <div class="col-xs-4">
+                                <a href="#1"><img src="<?php echo $url_path ?>/images/a1.jpg" class="img-responsive" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-xs-4">
+                                <a href="#1"><img src="<?php echo $url_path ?>/images/a2.jpg" class="img-responsive" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-xs-4">
+                                <a href="#1"><img src="<?php echo $url_path ?>/images/a3.jpg" class="img-responsive" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-xs-4">
+                                <a href="#1">
+                                    <img src="<?php echo $url_path ?>/images/a4.jpg" class="img-responsive" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-xs-4">
+                                <a href="#1">
+                                    <img src="<?php echo $url_path ?>/images/a5.jpg" class="img-responsive" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-xs-4">
+                                <a href="#1">
+                                    <img src="<?php echo $url_path ?>/images/a6.jpg" class="img-responsive" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <!-- add  more items here -->
+                        <!-- Example item start:  -->
+
+                        <div class="item">
+                            <div class="col-xs-4">
+                                <a href="#1">
+                                    <img src="<?php echo $url_path ?>/images/a7.jpg" class="img-responsive" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <a class="left carousel-control" href="#theCarousel" data-slide="prev">
+                    <i class="fa fa-chevron-left"> </i>
+                </a>
+                <a class="right carousel-control" href="#theCarousel" data-slide="next">
+                    <i class="fa fa-chevron-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
