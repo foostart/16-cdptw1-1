b2b6b8d2d4e80a8f384386470c097de170c72bd9
quochoa1999 <lehoa422@gmail.com>
2019-09-24 15:50:28 +0700

    commit 702 v3

--- 702/702.php
+++ 702/702.php
@@ -1,22 +1,19 @@
 <?php
-  $url_host = 'http://' . $_SERVER['HTTP_HOST'];
-  $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
-  $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-
-  preg_match_all($pattern_uri, __DIR__, $matches);
-  $url_path = $url_host . $matches[1][0];
-  $url_path = str_replace('\\', '/', $url_path);
-
-  if (!class_exists('lessc')) {
-      $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
-      require_once($dir_block . '/libs/lessc.inc.php');
-  }
-  $less = new lessc;
-  $less->compileFile('less/702.less', 'css/702.css');
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+if (!class_exists('lessc')) {
+$dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+require_once($dir_block . '/libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/702.less', 'css/702.css');
 ?>
 <!DOCTYPE html>
 <html>
-
     <head>
         <title>702</title>
         <meta charset="UTF-8">
@@ -25,45 +22,54 @@
         <link href="<?php echo $url_path ?>/css/font-awesome.min-4.6.3.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $url_path ?>/css/swiper.min-3.4.2.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $url_path ?>/css/702.css" rel="stylesheet" type="text/css"/>
-
         <script src="<?php echo $url_path ?>/js/swiper.min.js"></script>
         <script src="<?php echo $url_path ?>/js/jquery-1.11.0.min.js"></script>
         <script src="<?php echo $url_path ?>/js/bootstrap.min.js" ></script>
-    </head> 
-
-    <body>                
+    </head>
+    <body>
         <?php include $dir_block . '/702-content.php'; ?>
-         <script>
-    var galleryTop = new Swiper('.gallery-top', {
-      spaceBetween: 30,
-      
-    });
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-      direction: 'vertical',
-      spaceBetween: 10,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      touchRatio: 0.2,
-      slideToClickedSlide: true,
-    });
-    galleryTop.controller.control = galleryThumbs;
-    galleryThumbs.controller.control = galleryTop;
-
-     var gallerybotton = new Swiper('.gallery-botton', {
-      slidesPerView: 3,
-      spaceBetween: 10,
-      slidesPerGroup: 3,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
+        <script>
+        var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 30,
+        
+        });
+        var galleryThumbs = new Swiper('.gallery-thumbs', {
+        direction: 'vertical',
+        spaceBetween: 10,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        touchRatio: 0.2,
+        slideToClickedSlide: true,
+        });
+        galleryTop.controller.control = galleryThumbs;
+        galleryThumbs.controller.control = galleryTop;
+        var gallerybotton = new Swiper('.gallery-botton', {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
         el: '.swiper-pagination',
         clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  </script>
+        },
+        nextButton: '#swiper_btn_next',
+        prevButton: '#swiper_btn_prev',
+        breakpoints: {
+        1200: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        },
+        990: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+        },
+        770: {
+        slidesPerView: 1,
+        spaceBetween: 60,
+        },
+        }
+        });
+        </script>
     </body>
 </html>
